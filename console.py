7600ffd7030e38def8fd19cfa58ee0bad6b054db
Ukeme Edet <ukemeedet2207@gmail.com>
2024-02-08 13:46:12 +0100

    Console 0.0.1

--- console.py
+++ console.py
@@ -4,11 +4,34 @@
 This is a simple console application that will allow the user to interact with
 the
 """
+import cmd
 
 
-def main():
-    pass
+class HBNBCommand(cmd.Cmd):
+    """
+    This is a simple console application that will allow the user to interact
+    with the
+    """
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """
+        Quit command to exit the program
+        """
+        return True
+
+    def do_EOF(self, arg):
+        """
+        EOF command to exit the program
+        """
+        return True
+
+    def emptyline(self):
+        """
+        Empty line
+        """
+        pass
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
