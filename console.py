d2ce800eeade7bebd03047558aa12c2e4177005d
Ukeme Edet <ukemeedet2207@gmail.com>
2024-02-11 14:19:40 +0100

    FIxing the console

--- console.py
+++ console.py
@@ -6,6 +6,26 @@ the models of the application
 """
 
 import cmd
+import re
+from shlex import split
+
+
+def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
 
 
 class HBNBCommand(cmd.Cmd):
@@ -136,6 +156,7 @@ class HBNBCommand(cmd.Cmd):
             instance = storage.all()[key]
             setattr(instance, args[2], args[3])
             instance.save()
+            storage.save()
 
     def do_count(self, arg):
         """
@@ -164,27 +185,19 @@ class HBNBCommand(cmd.Cmd):
             "show": self.do_show,
             "destroy": self.do_destroy,
             "count": self.do_count,
-            "update": self.do_update,
+            "update": self.do_update
         }
-        dot_index = arg.find(".")
-        if dot_index != -1:
-            argl = [arg[:dot_index], arg[dot_index + 1:]]
-            open_paren_index = argl[1].find("(")
-            close_paren_index = argl[1].find(")")
-            if (
-                open_paren_index != -1
-                and close_paren_index != -1
-                and open_paren_index < close_paren_index
-            ):
-                command = [
-                    argl[1][:open_paren_index],
-                    argl[1][open_paren_index + 1: close_paren_index],
-                ]
+        match = re.search(r"\.", arg)
+        if match is not None:
+            argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
+            match = re.search(r"\((.*?)\)", argl[1])
+            if match is not None:
+                command = [argl[1][:match.span()[0]], match.group()[1:-1]]
                 if command[0] in argdict.keys():
                     call = "{} {}".format(argl[0], command[1])
                     return argdict[command[0]](call)
-
         print("*** Unknown syntax: {}".format(arg))
+        return False
 
 
 if __name__ == "__main__":
